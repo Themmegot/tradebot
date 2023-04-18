6df581d2a1d7d745a0e4893d8d2371090faa193d
Themmegot <60691022+Themmegot@users.noreply.github.com>
2023-04-18 16:00:49 +0200

    Added description

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+# Passphrase used in the json file to protect the webbhook from unauthorized trade entries
+# Would also recommend to allow only the tradingview ip-adresses to allow access to the webhook post function
 WEBHOOK_PASSPHRASE = "<your passphrase>"
 
+# API KEY/SECRET from binance. Create on for the bot, and also allow only tradingview ip adresses to use this API pair
 API_KEY = '<your binance api key>'
 API_SECRET = '<your binance api secret>'
