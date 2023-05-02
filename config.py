2f34d5e5fcf8fbf61bb186b82d13942403f36cd4
Themmegot <60691022+Themmegot@users.noreply.github.com>
2023-05-02 11:42:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Would also recommend to allow only the tradingview ip-adresses to allow access to the webhook post function
 WEBHOOK_PASSPHRASE = "<your passphrase>"
 
-# API KEY/SECRET from binance. Create on for the bot, and also allow only tradingview ip adresses to use this API pair
+# API KEY/SECRET from binance. Create one for the bot, and also allow only tradingview ip adresses to use this API pair
 API_KEY = '<your binance api key>'
 API_SECRET = '<your binance api secret>'
